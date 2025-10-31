2d6997a6c53453a3e670f20a4cb15e95955f3d22
Melissa <melissahuerta.dev@gmail.com>
2025-10-31 15:55:19 -0500

    feat: global ui con botones agregado

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -10,6 +10,11 @@ func _on_play_button_pressed() -> void:
 	#print("Game started")
 	GlobalManager.audio_manager.play_game_click_sfx()
 	GlobalManager.audio_manager.play_game_music()
+	
+		# Mostrar el corazón en la UI global
+	for node in get_tree().get_nodes_in_group("ui_layer"):
+		node.show_heart_button()
+	
 	get_tree().change_scene_to_file("res://scenes/Pantalla0_Inicio.tscn")
 
 #func _on_quit_button_pressed() -> void:
--- scripts/global_ui.gd
+++ scripts/global_ui.gd
@@ -8,18 +8,17 @@ var music_volume := 1.0
 var sfx_volume := 1.0
 
 func _ready():
-	print("ready")
+	add_to_group("ui_layer") # permite acceder desde otras escenas
 	build_controls()
 	
 func build_controls():
-	print(".. build controls")
 	# Lista de los elementos a agregar
 	var items_data = [
 		{ "name": "QuitButton",  "icon": "res://assets/ui/globals/logout.svg", "callback": func(): _on_quit_button_pressed(), "side": "left" },
 		{ "type": "spacer" }, # separador flexible
 		{ "name": "HeartButton", "icon": "res://assets/ui/globals/heart.svg", "callback": func(): _on_heart_button_pressed(), "side": "right" },
 		{ "type": "gap", "width": 12 }, # espacio fijo
-		{ "name": "SoundButton", "icon": "res://assets/ui/globals/sound.svg", "callback": func(): _on_sound_button_pressed(), "side": "right" }
+		#{ "name": "SoundButton", "icon": "res://assets/ui/globals/sound.svg", "callback": func(): _on_sound_button_pressed(), "side": "right" }
 	]
 	
 	for item in items_data:
@@ -30,14 +29,16 @@ func build_controls():
 				"gap":
 					add_gap(item.width)
 		else:
-			add_button(item.name, item.icon, item.callback)
+			var btn = add_button(item.name, item.icon, item.callback)
+			if item.name == "HeartButton":
+				btn.visible = false
+			#add_button(item.name, item.icon, item.callback)
 
 func add_spacer():
 	# Espaciador
 	var spacer = Control.new()
 	spacer.size_flags_horizontal = Control.SIZE_EXPAND
 	spacer.size_flags_vertical = Control.SIZE_FILL
-
 	control_container.add_child(spacer)
 	
 func add_gap(width: int):
@@ -46,10 +47,11 @@ func add_gap(width: int):
 	gap.custom_minimum_size = Vector2(width, 0)
 	control_container.add_child(gap)
 	
-func add_button(button_name: String, icon_path: String, callback: Callable):
+func add_button(button_name: String, icon_path: String, callback: Callable) -> TextureButton:
 	var btn = create_button(button_name, icon_path, callback)
 	control_container.add_child(btn)
-	print("%s was added to scene" % button_name)
+	#print("%s was added to scene" % button_name)
+	return btn
 
 func create_button(button_name: String, icon_path: String, callback: Callable) -> TextureButton:
 	var btn: TextureButton = UIButtonScene.instantiate()
@@ -58,20 +60,23 @@ func create_button(button_name: String, icon_path: String, callback: Callable) -
 	btn.ignore_texture_size = true  # Permite escalar manualmente
 	btn.custom_minimum_size = Vector2(42, 42)
 	btn.pressed.connect(callback)
-	print("creating button")
 	return btn
+	
+func show_heart_button():
+	var heart = control_container.get_node_or_null("HeartButton")
+	if heart:
+		heart.visible = true
 
 func _on_heart_button_pressed() -> void:
 	var credits_modal_instance = preload("res://scenes/modals/CreditsModal.tscn").instantiate()
 	add_child(credits_modal_instance)
 	credits_modal_instance.popup_centered()
 
+# Mostrar modal de opciones de sonido
 func _on_sound_button_pressed() -> void:
-	# Mostrar modal de opciones de sonido
-	#get_tree().paused = true
+	get_tree().paused = true
 	print("TO DO sound was pressed")
 	#$ModalMusica.show()
 
 func _on_quit_button_pressed() -> void:
-	#get_tree().quit()
-	print("TO DO quit was pressed")
+	get_tree().quit()
